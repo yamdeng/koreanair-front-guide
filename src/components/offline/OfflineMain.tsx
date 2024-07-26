19b8d937f6f429189218b51cce88b85527616def
user1 <jinwon.bae.job@gmail.com>
2024-07-26 16:51:57 +0900

    update

---
+++ src/components/offline/OfflineMain.tsx
@@ -0,0 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
+export default function OfflineMain() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="wrap">
+      <div>
+        Main
+      </div>
+    </div>
+  );
+}
