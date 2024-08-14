b8af30a9e747cfa62117fda3703a412ec60d1a1a
안용성 <pj.yamdeng2@kalmate.net>
2024-08-14 17:12:10 +0900

    TODO 반영

---
+++ src/components/layout/Navigation.tsx
@@ -0,0 +1,11 @@
+// #.local navigation
+// <Navigation />
+//   1.menuId 추출
+//   2.title recursive 하게 추출
+//   3.진입시 권한 체크도 같이 함
+
+function Navigation() {
+  return <div></div>;
+}
+
+export default Navigation;
