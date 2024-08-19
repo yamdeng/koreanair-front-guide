a960c56219c084d964f4ff15043a185fcf777b62
yamdeng <yamdeng@gmail.com>
2024-08-19 10:30:30 +0900

    toast-service guide

--- src/components/guide/GuideToastService.tsx
+++ src/components/guide/GuideToastService.tsx
@@ -1,4 +1,33 @@
+import ToastService from '@/services/ToastService';
+import AppNavigation from '../common/AppNavigation';
+
 function GuideToastService() {
-  return <div>GuideToastService</div>;
+  const handleToastService = () => {
+    ToastService.success('call success');
+    ToastService.error('call error');
+    ToastService.warn('call warn');
+    ToastService.info('call info.');
+  };
+
+  return (
+    <>
+      <AppNavigation />
+      <div className="conts-title">
+        <h2>ToastService</h2>
+      </div>
+      <div className="editbox">
+        <div className="btn-area">
+          <button
+            type="button"
+            name="button"
+            className="btn-sm btn_text btn-darkblue-line"
+            onClick={handleToastService}
+          >
+            toast
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 export default GuideToastService;
