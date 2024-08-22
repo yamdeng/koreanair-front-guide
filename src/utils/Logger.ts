ee8d0dc9a650e7e56ffb43092ba002cf90202113
yamdeng <yamdeng@gmail.com>
2024-08-22 20:34:45 +0900

    Logger add

---
+++ src/utils/Logger.ts
@@ -0,0 +1,40 @@
+const Logger = {
+  debug: function (message) {
+    // eslint-disable-next-line
+    console.debug(message);
+  },
+  info: function (message) {
+    // eslint-disable-next-line
+    console.info(message);
+  },
+  warn: function (message) {
+    // eslint-disable-next-line
+    console.warn(message);
+  },
+  error: function (message) {
+    // eslint-disable-next-line
+    console.error(message);
+    try {
+      //   const appStore = rootStore.appStore;
+      //   const uiStore = rootStore.uiStore;
+      //   const errorDoc = {};
+      //   errorDoc.token = appStore.token;
+      //   errorDoc.version = Config.version;
+      //   errorDoc.message = message;
+      //   errorDoc.created = moment().format('YYYY-MM-DD HH:mm:ss');
+      //   errorDoc.currentRouteUrl = uiStore.currentRouteUrl || '';
+      //   errorDoc.beforeRouteUrl = uiStore.beforeRouteUrl || '';
+      //   errorDoc.userAgent = navigator.userAgent || '';
+      //   errorDoc.message = message.substr(0, 2500);
+    } catch (e) {
+      // eslint-disable-next-line
+      console.error('Logger error : ' + JSON.stringify(e));
+    }
+  },
+  log: function (message) {
+    // eslint-disable-next-line
+    console.log(message);
+  },
+};
+
+export default Logger;
